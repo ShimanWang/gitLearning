8a411b3a7ac85a39b2762da19dd59de227ac75fb
wangshiman <wangshiman1995@163.com>
2020-01-07 20:11:26 +0800

    小张开发的内容

--- src/test/java/com/gitLearning/AppTest.java
+++ src/test/java/com/gitLearning/AppTest.java
@@ -12,5 +12,9 @@ public class AppTest
         System.out.println("hello world");
         System.out.println("在分支上开发");
         System.out.println("小张开发的内容");
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
     }
 }
